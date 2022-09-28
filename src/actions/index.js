b499686c64d9030ee826aa617d7bc6500ccad431
Guillaume-Ln <guillaume-lanoe@hotmail.fr>
2022-09-28 11:51:39 +0200

    :sparkles: feat: controlled input connection added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-// TODO use act snippet;
+export const CHANGE_INPUT_LOGIN_CONNEXION_VALUE = 'CHANGE_INPUT_LOGIN_CONNEXION_VALUE';
+export const CHANGE_INPUT_PASSWORD_CONNEXION_VALUE = 'CHANGE_INPUT_PASSWORD_CONNEXION_VALUE';
+
+export const actionChangeInputLoginConnexionValue = (newValue) => ({
+  type: CHANGE_INPUT_LOGIN_CONNEXION_VALUE,
+  value: newValue,
+
+  
+});
+
+export const actionChangeInputPasswordConnexionValue = (newValue) => ({
+  type: CHANGE_INPUT_PASSWORD_CONNEXION_VALUE,
+  value: newValue,
+
+  
+});
