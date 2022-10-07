c7e2a94ef99a88edd9799ef7ef7109f206c7d694
Arthur Moignet <moignetart@hotmail.fr>
2022-10-07 10:31:07 +0200

    :sparkles: added 404error in react

--- src/components/Error404Page/index.js
+++ src/components/Error404Page/index.js
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import './style.scss';
+import img404 from 'src/assets/images/error404background.jpg';
 
 function Error404() {
   return (
@@ -6,7 +8,8 @@ function Error404() {
       <section className="main-error">
         <div className="main-error-404-glow">
           <p>Oops it's a 404 !</p>
-          <p>Go back to home page.</p>
+          <p>Go back to <Link to="/" className="underline">home page.</Link></p>
+          <img className="img404" src={img404} alt="background 404" />
         </div>
       </section>
     </main>
