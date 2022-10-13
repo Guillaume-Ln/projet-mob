de90bfc979de06a0ea9f83a7535a678dd515fe90
Arthur Moignet <moignetart@hotmail.fr>
2022-10-13 16:44:09 +0200

    error404 fixed

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,7 +7,7 @@ import Header from '../Header';
 import Footer from '../Footer';
 import Login from '../Login';
 import Signup from '../Signup';
-/* import Error404 from '../Error404Page'; */
+import Error404 from '../Error404Page';
 import Home from '../Home';
 import CreateTournament from '../CreateTournament';
 import Tournaments from '../Tournaments';
@@ -49,7 +49,7 @@ function App() {
       {signinIsVisible && <Login />}
       {signupIsVisible && <Signup />}
       <Routes>
-        {/*         <Route path="*" element={<Error404 />} /> */}
+        <Route path="*" element={<Error404 />} />
         <Route path="/tournaments" element={<Tournaments />} />
         <Route path="/newtournament" element={<CreateTournament />} />
         <Route path="/mytournaments" element={<MyTournaments />} />
